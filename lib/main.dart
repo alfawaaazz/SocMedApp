2785404819c3b925952ea65e74a3f3a60cbaf2cc
Jay Benedict Alfaras <alfarasjb@gmail.com>
2020-11-26 18:32:36 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:SocMedApp/screens/homescreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ConnectR',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
